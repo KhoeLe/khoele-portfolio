27941a8bfba3ed913d764fc6a1472f8e1fee7855
KLL\vn.ngockhoe.le <ngockhoe.le@kepland.com.vn>
2024-04-16 14:44:35 +0700

    #imp : Add @vercel/analytics package and import Analytics component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import GridSmallBackground from "@/components/gridsmall-background";
 import { TracingBeam } from "@/components/tracing-beam";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"], display: 'swap', });
 
@@ -58,6 +59,7 @@ export default function RootLayout({
         <AOSInit />
       </body>
       <GoogleAnalytics gaId="G-VK5QGET3FZ" />
+      <Analytics />
     </html>
   );
 }
